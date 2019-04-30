da9263e47a9ff75e44868a67e5cebf93a029024c
hardfist <1562502418@qq.com>
2019-04-30 13:26:53 +0800

    add sample-heap-profile
    Change-Id: I1c50ceff4f82ca96e94dcf200d6918786aaf3220

---
+++ examples/memory/heapsnapshot/sample-heap-profile.js
@@ -0,0 +1,31 @@
+const heapProfile = require('heap-profile');
+
+heapProfile.start();
+
+require('heapdump')
+const leaks = [];
+
+class LeakingClass {
+  constructor() {
+    this.name = Math.random().toString(36);
+    this.age = Math.floor(Math.random() * 100);
+  }
+}
+function leak1(){
+   for (let i = 0; i < 100000; i++) {
+    leaks.push(new LeakingClass());
+  }
+}
+function leak2(){
+   for (let i = 0; i < 500000; i++) {
+    leaks.push(new LeakingClass());
+  }
+}
+setInterval(function pushleak(){
+  leak1();
+  leak2();
+  heapProfile.write((err, filename) => {
+    console.log(`heapProfile.write. err: ${err} filename: ${filename}`);
+  });
+  console.warn("Leaks: %d", leaks.length);
+}, 5000);
